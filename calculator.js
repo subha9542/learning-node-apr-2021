993417590324ab18334bfefbd913dc7e8c2c127e
admin <example@example.com>
2021-04-06 21:45:34 +0530

    added coded files.

---
+++ calculator.js
@@ -0,0 +1,31 @@
+const argument1 = parseInt(process.argv[2], 10)
+const operator = process.argv[3]
+const argument2 = parseInt(process.argv[4], 10)
+
+console.log(operator);
+
+switch (operator) {
+    case "+":
+        console.log(argument1 + argument2)
+        break;
+
+    case "-":
+        console.log(argument1 - argument2)
+        break;
+
+    case "x":
+        console.log(argument1 * argument2)
+        break;
+
+    case "/":
+        console.log(argument1 / argument2)
+        break;
+
+    default:
+        break;
+}
+
+// node calculator.js 11454 + 2454
+// node calculator.js 11454 - 2454
+// node calculator.js 11454 * 2454
+// node calculator.js 11454 / 2454
