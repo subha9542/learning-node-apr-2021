9ab8284000d1c81297128062c0f4b32f924a40ec
admin <example@example.com>
2021-04-07 20:47:57 +0530

    express api to store and get employee.

--- session-2/server.js
+++ session-2/server.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const app = express(); // application
+const fs = require("fs");
 
-const employee = {
-    name : "xyz",
-    age : 45,
-    tech : ["angular", "react", "node"],
-    talk : function(){}
-};
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.get("/", (req, res)=>{
-    // res.send("hello world!!!") // HTML or Text
-    res.json(employee) // JSON
+app.get("/employees", (req, res) => {
+    fs.readFile("./session-2/employee.json", (error, response) => {
+        if (error) {
+            return false;
+        }
+        const contentString = response.toString();
+        const contentJSON = JSON.parse(contentString)
+        res.json(contentJSON) // JSON
+        // res.json(JSON.parse(response.toString()))
+    })
+
 })
 
-app.listen(3000, ()=>{
+app.post("/employees", (req, res) => {
+    fs.readFile("./session-2/employee.json", (error, response) => {
+        if (error) {
+            return false;
+        }
+        const contentString = response.toString();
+        const contentJSON = JSON.parse(contentString)
+        const lastItem = contentJSON[contentJSON.length - 1]
+        const newItemToBeAdded = req.body;
+        newItemToBeAdded.id = lastItem.id + 1
+        contentJSON.push(newItemToBeAdded);
+        fs.writeFile("./session-2/employee.json", JSON.stringify(contentJSON), () => {
+            res.json(newItemToBeAdded) // JSON
+        })
+    })
+
+})
+
+app.listen(3000, () => {
     console.log("application server started at 3000.")
 })
 
