78ad136167a0f17f6f4652f92fce4bec31216148
admin <example@example.com>
2021-04-07 22:26:54 +0530

    async await.

--- session-3/async-await.js
+++ session-3/async-await.js
@@ -1,37 +1,37 @@
 const { setTimeoutPromisified } = require("./promise-creator")
 
 async function triggerTimer() {
-    // console.log("before");
-    // await setTimeoutPromisified(1000);
-    // console.log("1");
-    // await setTimeoutPromisified(1000);
-    // console.log("2");
-    // await setTimeoutPromisified(1000);
-    // console.log("3");
-    // await setTimeoutPromisified(1000);
-    // console.log("4");
-    // await setTimeoutPromisified(1000);
-    // console.log("5");
-    // await setTimeoutPromisified(1000);
-    // console.log("6");
-    // await setTimeoutPromisified(1000);
-    // console.log("after");
-
     console.log("before");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("1");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("2");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("3");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("4");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("5");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("6");
-    setTimeoutPromisified(1000);
+    await setTimeoutPromisified(1000);
     console.log("after");
+
+    // console.log("before");
+    // setTimeoutPromisified(1000);
+    // console.log("1");
+    // setTimeoutPromisified(1000);
+    // console.log("2");
+    // setTimeoutPromisified(1000);
+    // console.log("3");
+    // setTimeoutPromisified(1000);
+    // console.log("4");
+    // setTimeoutPromisified(1000);
+    // console.log("5");
+    // setTimeoutPromisified(1000);
+    // console.log("6");
+    // setTimeoutPromisified(1000);
+    // console.log("after");
 
     
 }
