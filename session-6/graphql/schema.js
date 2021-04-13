9499f28746b2cc214a7944b9c637ebf3738bcdf0
admin <example@example.com>
2021-04-13 21:02:05 +0530

    Changes for Order and Orders List

--- session-6/graphql/schema.js
+++ session-6/graphql/schema.js
@@ -1,33 +1,50 @@
 const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 
-
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const orders = [
+    { name: "abc", email: "abc@def.com", address: "abc def" },
+    { name: "def", email: "abc@def.com", address: "abc def" },
+    { name: "rtg", email: "abc@def.com", address: "abc def" },
+    { name: "aws", email: "abc@def.com", address: "abc def" }
+]
 
 const OrderType = new GraphQLObjectType({
-    name : 'Order',
-    fields : {
-        name : { type : GraphQLString },
-        address : { type : GraphQLString },
-        email : { type : GraphQLString } 
+    name: 'Order',
+    fields: {
+        name: { type: GraphQLString },
+        address: { type: GraphQLString },
+        email: { type: GraphQLString }
     }
 })
 
 const Root = new GraphQLObjectType({
-    name : "Root",
-    fields : {
-        orders : {
-            type : OrderType,
-            resolve(){
+    name: "Root",
+    fields: {
+        order: {
+            type: OrderType,
+            args : { name : { type : GraphQLString } },
+            resolve(context, args) {
                 // Mongo
-                return { name : "abc", email : "abc@def.com", address : "abc def" }
+                console.log(args, orders);
+                return orders.find((order)=>{
+                    return order.name === args.name;
+                })
             }
-
+        },
+        orders: {
+            type: GraphQLList(OrderType),
+            resolve() {
+                // Mongo
+                return orders
+            }
         }
     }
 })
 
 module.exports = new GraphQLSchema({
-    query : Root
+    query: Root
 })
 
 // npm install express express-graphql graphql axios lodash
+// node index.js
+// http://localhost:3000/graphql
