ec5bdb996c7b16a68693d3d9e987ff30321a3897
admin <example@example.com>
2021-04-14 22:21:12 +0530

    API

--- session-7/index.js
+++ session-7/index.js
@@ -5,12 +5,19 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken")
 
-const PeopleModel = require("./schema/users.model")
+// Fix for CORS issue.
+const cors = require("cors");
 
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }));
+
+// Fix for CORS issue.
+app.use(cors())
+
+
+const PeopleModel = require("./schema/users.model")
 
 
 app.set("view engine", "ejs");
@@ -32,9 +39,26 @@ app.post("/login", async (req, res) => {
             res.status(401).json({ message: "not authorzied" })
             return false;
         }
-
     }
     res.status(401).json({ message: "not authorzied" })
+})
+
+app.get("/whoami", async (req, res)=>{
+    const token = req.headers.authorization || null;
+    if(!token) {
+        res.status(401).json({ message: "not authorzied" })
+        return false;
+    }
+    // refer line > jwt.sign({ 
+    try {
+        const decoded = jwt.verify(token, process.env.APPLICATION_SECRET_KEY);
+        const user = await PeopleModel.findOne({ id: decoded.id }, "-password -_id -address -company")
+        res.json(user)
+        return true;
+    } catch (error) {
+        res.status(401).json({ message: "not authorzied" })
+        return false;
+    }
 })
 
 app.listen(3600, () => {
